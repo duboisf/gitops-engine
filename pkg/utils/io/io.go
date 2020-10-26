4f6dc3ce394525b944f1cf33aab173293776413d
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2020-10-26 12:27:03 -0700

    Draft

--- pkg/utils/io/io.go
+++ pkg/utils/io/io.go
@@ -2,16 +2,10 @@ package io
 
 import (
 	"os"
-
-	"k8s.io/klog/v2/klogr"
 )
 
 var (
-	TempDir   string
-	NopCloser = NewCloser(func() error {
-		return nil
-	})
-	Logger = klogr.New()
+	TempDir string
 )
 
 func init() {
@@ -27,28 +21,4 @@ func DeleteFile(path string) {
 		return
 	}
 	_ = os.Remove(path)
-}
-
-type Closer interface {
-	Close() error
-}
-
-type inlineCloser struct {
-	close func() error
-}
-
-func (c *inlineCloser) Close() error {
-	return c.close()
-}
-
-func NewCloser(close func() error) Closer {
-	return &inlineCloser{close: close}
-}
-
-// Close is a convenience function to close a object that has a Close() method, ignoring any errors
-// Used to satisfy errcheck lint
-func Close(c Closer) {
-	if err := c.Close(); err != nil {
-		Logger.Error(err, "Failed to close %v", c)
-	}
 }
